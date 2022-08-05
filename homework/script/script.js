4219dd90ee6a82f3c55d08cd09c94fd6bf2f70d4
Tomori <kostya.papushnikov@yandex.ru>
2022-08-05 14:16:59 +0700

    Menu
    add menu and open/close script

---
+++ homework/script/script.js
@@ -0,0 +1,7 @@
+function ClickFunction() {
+    document.getElementById("mega_box").style.visibility = "visible"
+}
+
+function CloseFunction() {
+    document.getElementById("mega_box").style.visibility = "hidden"
+}
